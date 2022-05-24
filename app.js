9299b1cc57f78ee361f8037f6435cf4d63e9f6a6
Anton Buldenkov <anton@buldenkov.ru>
2022-05-24 13:58:13 +0300

    add auth middleware for routes & 404 page

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
 const { celebrate, errors } = require('celebrate');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
+const { auth } = require('./middlewares/auth');
 const { handleError } = require('./middlewares/errors');
+const { NotFoundError } = require('./errors/notFound');
 const { signinSchema, signupSchema } = require('./middlewares/validator');
 
 const { login, createUser } = require('./controllers/userController');
@@ -15,6 +18,7 @@ mongoose.connect('mongodb://localhost:27017/bitfilmsdb');
 const app = express();
 const { PORT = 3000 } = process.env;
 
+app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
@@ -22,6 +26,8 @@ app.use(requestLogger);
 
 app.post('/signin', celebrate({ body: signinSchema }), login);
 app.post('/signup', celebrate({ body: signupSchema }), createUser);
+
+app.use('*', auth, (req, res, next) => next(new NotFoundError('Страница не найдена')));
 
 app.use(errorLogger);
 app.use(errors());
---
+++ middlewares/auth.js
@@ -0,0 +1,21 @@
+const { NODE_ENV, JWT_SECRET } = process.env;
+const jwt = require('jsonwebtoken');
+const { UnauthorizedError } = require('../errors/unauthorized');
+
+module.exports.auth = (req, res, next) => {
+  const { jwtToken } = req.cookies;
+  if (!jwtToken) {
+    return next(new UnauthorizedError('Необходима авторизация'));
+  }
+
+  let payload;
+
+  try {
+    payload = jwt.verify(jwtToken, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
+  } catch (err) {
+    return next(new UnauthorizedError('Необходима авторизация'));
+  }
+
+  req.user = payload;
+  return next();
+};
