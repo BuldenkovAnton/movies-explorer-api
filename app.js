b74a046f57ec030ea19e4af252c781bcd5c0d8ed
Anton Buldenkov <anton@buldenkov.ru>
2022-05-24 10:06:41 +0300

    add requests logger

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 
 const bodyParser = require('body-parser');
 
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { handleError } = require('./middlewares/errors');
 
 mongoose.connect('mongodb://localhost:27017/bitfilmsdb');
@@ -13,6 +14,9 @@ const { PORT = 3000 } = process.env;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.use(requestLogger);
+
+app.use(errorLogger);
 app.use(handleError);
 
 app.listen(PORT);
