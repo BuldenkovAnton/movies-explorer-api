2cd7c9d2866520182d59c308ee7ceea17227c18e
Anton Buldenkov <anton@buldenkov.ru>
2022-05-26 14:01:23 +0300

    fix 500 error message

--- middlewares/errors.js
+++ middlewares/errors.js
@@ -4,7 +4,7 @@ module.exports.handleError = (err, req, res, next) => {
   const { statusCode = 500, message } = err;
   res
     .status(statusCode)
-    .send({ message: message === 500 ? SERVER_ERROR_TEXT : message });
+    .send({ message: statusCode === 500 ? SERVER_ERROR_TEXT : message });
 
   next();
 };
